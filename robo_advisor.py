b46ee21e9f6233f01017d93bf3e900d2e6d4d0cd
hirokiden <hd1043@stern.nyu.edu>
2019-06-16 17:06:55 -0400

    current date attached to last day, close, high, low, volume traded, selected symbol fixed as well

--- robo_advisor.py
+++ robo_advisor.py
@@ -24,6 +24,7 @@ python robo_advisor.py
 
 import requests
 import json # need this to import json string into dictionary
+from datetime import datetime #Taken from stackoverflow
 
 
 # Also, we know that we are working with $ pricing so let's get the formatting out of the way
@@ -43,12 +44,16 @@ parsed_response = json.loads(response.text) #this converts string format into di
 
 dates_list = list(parsed_response["Time Series (Daily)"].keys()) # convert the dates dictionary keys into list format
 
+
 current_date = dates_list[0] # son loads will alphavantage's most recent values that is already sorted, but might need to sort just in case later
+
+traded_stock_ticker = parsed_response["Meta Data"]["2. Symbol"]
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 most_recent_close = parsed_response["Time Series (Daily)"][current_date]["4. close"]
-
-
+most_recent_high = parsed_response["Time Series (Daily)"][current_date]["2. high"]
+most_recent_low = parsed_response["Time Series (Daily)"][current_date]["3. low"]
+most_recent_total_volumes_traded = parsed_response["Time Series (Daily)"][current_date]["5. volume"]
 
 
 # To find out what the keys are in dictionary format, remember to use command ' parsed_response.keys() '
@@ -61,7 +66,12 @@ most_recent_close = parsed_response["Time Series (Daily)"][current_date]["4. clo
 # However, realize the '["2019-06-14"]' is anchored and will be irrelevant tomorrow! so replace that as variable
 # Set a variable to dynamic function  
 # ' parsed_response["Time Series (Daily)"].keys() ' to find the dictionary keys of 'Time series Daily'
-# Conver the Time series Daily' dictinoary keys into a list so we can do as simply do current_date[0] to pull the first date
+# Convert the Time series Daily' dictionary keys into a list so we can do as simply do current_date[0] to pull the first date
+# With this variable now configured, we can now simply copy paste each of the following ranging from
+    # a) most_recent_close 
+    # b) most_recent_high
+    # c) most_recent_low
+
 
 
 
@@ -80,15 +90,16 @@ most_recent_close = parsed_response["Time Series (Daily)"][current_date]["4. clo
 
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {traded_stock_ticker}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT:", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {usd_format(float(most_recent_close))}") # a float() is necessary to convert a string to a float, otherwise error
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT HIGH: {usd_format(float(most_recent_high))}")
+print(f"RECENT LOW: {usd_format(float(most_recent_low))}")
+print(f"VOLUMES TRADED: {int(most_recent_total_volumes_traded)}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
